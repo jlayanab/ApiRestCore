ec658cdecb97d7f38e5abd89f53e8dd2a66dc570
jlayanab <jlayanab@icloud.com>
2021-10-25 12:19:24 -0500

    Add project files.

---
+++ ApiRestCore/Models/History.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRestCore.Models
+{
+    public class History
+    {
+        public int His_ID {get; set;}
+        public DateTime His_DateIn { get; set; }
+        public DateTime His_DateOut { get; set; }
+        public string His_Status { get; set; }
+
+    }
+
+}
